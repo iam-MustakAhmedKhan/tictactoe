a3f15ed5c2e3e9c1204e4459a1f9f560b267cdb1
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2024-01-10 12:59:55 +0600

    winner functionalites done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import { Square } from "./Square";
+import { calculateWinner } from "../utils/calculateWinner";
 
 const Board = () => {
 
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
+    const winner = calculateWinner(squares)
+    let status;
+    if (winner) {
+        status =`Winner ${winner}`
+    } else {
+        status=`Next player ${xIsNext?"X":"O"}`
+    }
 
     const handleClick = (i) => {
+
+        if (squares[i]||calculateWinner(squares)) {
+            return;
+        }
+
         const nextSquares = squares.slice();
 
         nextSquares[i] = xIsNext?"X":"O";
@@ -15,11 +28,20 @@ const Board = () => {
     }
 
     return (
-        <div className="grid grid-cols-3">
-            {squares.map((a, i) => (
-                <Square key={i} value={a} onSquareClick={()=>handleClick(i)} />
-            ))}
-        </div>
+        <>
+            <div className="text-3xl text-white">
+                {status}
+            <div className="grid grid-cols-3">
+                {squares.map((a, i) => (
+                    <Square
+                        key={i}
+                        value={a}
+                        onSquareClick={() => handleClick(i)}
+                    />
+                ))}
+            </div>
+            </div>
+        </>
     );
 }
 
